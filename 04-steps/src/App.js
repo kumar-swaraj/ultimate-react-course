794158343ee4685eb4f0d6b4c2dad1d7183a3090
kumar-swaraj <kumarswaraj511@gmail.com>
2023-12-18 17:06:55 +0530

    06 State, Events, and Forms: Interactive Components -- {updating state based on current value of state --- use callback fn}

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -18,11 +18,12 @@ function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
+    // always use callback function when updating state based on current value of state --- (s) => s - 1
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
 
     /*
     // BAD PRACTICE
@@ -34,7 +35,7 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setIsOpen(!isOpen)} className="close">
+      <button onClick={() => setIsOpen((is) => !is)} className="close">
         &times;
       </button>
       {isOpen && (
