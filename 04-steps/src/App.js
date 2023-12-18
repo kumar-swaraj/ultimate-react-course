d9ef2559b28afb0d0697612db53b1554f32f67c3
kumar-swaraj <kumarswaraj511@gmail.com>
2023-12-18 12:30:04 +0530

    06 State, Events, and Forms: Interactive Components -- {handling events the react way}

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,6 +7,14 @@ const messages = [
 function App() {
   let step = 1;
 
+  function handlePrevious() {
+    alert('previous');
+  }
+
+  function handleNext() {
+    alert('next');
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,11 +28,17 @@ function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
 
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
