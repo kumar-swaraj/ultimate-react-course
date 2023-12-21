7b9decb710c1eecfa6975d7bf0db5fb1b5955b72
kumar-swaraj <kumarswaraj511@gmail.com>
2023-12-21 20:05:44 +0530

    07 -- {travel-list - thinking about state & lifting state up}

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +27,7 @@ function Logo() {
   return <h1>🏝️ Far away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -31,7 +37,8 @@ function Form() {
     if (!description) return;
 
     const newItem = { id: Date.now(), description, quantity, packed: false };
-    console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription('');
     setQuantity(1);
@@ -59,11 +66,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
