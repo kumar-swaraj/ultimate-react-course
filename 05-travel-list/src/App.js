24c0daa9063e1b6e40c6a2a1df986a29354eb75f
kumar-swaraj <kumarswaraj511@gmail.com>
2023-12-22 12:09:33 +0530

    07 -- {travel-list - calculating statistics as derived state}

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -109,10 +109,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.reduce((acc, cur) => (cur.packed ? acc + 1 : acc), 0);
+  const percentagePacked = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentagePacked === 100
+          ? 'You got everything! Ready to go ✈️'
+          : `💼 You have ${numItems} items on your list, and you already packed 
+        ${numPacked} (${percentagePacked || 0}%)`}
+      </em>
     </footer>
   );
 }
