0a2023d8cc3192e076f647989fe6e0e2dac89a67
kumar-swaraj <kumarswaraj511@gmail.com>
2023-12-22 23:56:10 +0530

    07 exercise-01 accordion-component-v1

---
+++ challenges/7 - Thinking In React State Management || 05-travel-list/exercise-1-accordion-component-v1/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './styles.css';
+
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <ul className="accordion">
+      {data.map((el, idx) => (
+        <AccordionItem
+          key={el.title}
+          num={idx + 1}
+          title={el.title}
+          text={el.text}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <li
+      onClick={() => setIsOpen((open) => !open)}
+      className={isOpen ? 'item open' : 'item'}
+    >
+      <p className="number">{String(num).padStart(2, 0)}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </li>
+  );
+}
