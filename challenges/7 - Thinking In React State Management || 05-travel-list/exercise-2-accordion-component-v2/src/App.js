cc435000218d0bcad0194203f5e6f27ad651171c
kumar-swaraj <kumarswaraj511@gmail.com>
2023-12-23 22:20:12 +0530

    07 exercise-02 accordion-component-v2

---
+++ challenges/7 - Thinking In React State Management || 05-travel-list/exercise-2-accordion-component-v2/src/App.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import './styles.css';
+
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+    <ul className="accordion">
+      {data.map((el, idx) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          key={el.title}
+          num={idx + 1}
+          title={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        key="test 1"
+        num={23}
+        title="Thinking in React"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
+    </ul>
+  );
+}
+
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+
+  return (
+    <li onClick={handleToggle} className={isOpen ? 'item open' : 'item'}>
+      <p className="number">{String(num).padStart(2, 0)}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && <div className="content-box">{children}</div>}
+    </li>
+  );
+}
